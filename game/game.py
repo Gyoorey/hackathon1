43764bcf4f012edb897113f31921b429db8d4185
Gyoorey <gyooorey@hotmail.com>
2018-07-29 23:21:04 +0200

    successful training

--- game/game.py
+++ game/game.py
@@ -7,13 +7,13 @@ import math
 import cv2
 
 H = 600
-W = 820
-num_of_cols = int(41) #should be even
-player_offset = 50
+W = 840
+num_of_cols = int(21) #should be even
+player_offset = 20
 player_y = player_offset
-playerwidth = 30
+playerwidth = W / num_of_cols
 playerspeed = W / num_of_cols #one column
-output_width = int(num_of_cols) #should be even
+output_width = int(11) #should be even
 half_out_width = int((output_width-1)/2)
 
 
@@ -22,11 +22,12 @@ class Game:
         self.x = 0
         self.col = int(round((num_of_cols-1)/2))
         self.asteroids = []
-        self.ast_thresh = 0.95
+        self.ast_thresh = 0.5
         self.iter_cnt = 0
-        self.ast_speed = 10
+        self.ast_speed = 40
         self.r = Random(0)
         self.ended = False
+        self.col_available = np.zeros((1,num_of_cols))
 
     def show(self):
         img = np.zeros((H,W,3), np.uint8)
@@ -76,6 +77,7 @@ class Game:
         
 
     def iterate(self, key):
+        self.r = Random(self.iter_cnt)
         if (self.ended):
             return (self.iter_cnt, self.ended)
         if key == -1 and self.col > 0:
@@ -87,7 +89,8 @@ class Game:
             self.asteroids[i][1] = self.asteroids[i][1] - self.ast_speed
         # check collision
         for asteroid in self.asteroids:
-            if abs(self.col - asteroid[0])<3 and asteroid[1] <= player_y + playerwidth:
+            #if abs(self.col - asteroid[0])<3 and asteroid[1] <= player_y + playerwidth:
+            if self.col == asteroid[0] and asteroid[1] <= player_y + playerwidth:
                 self.ended = True
                 return (self.iter_cnt, self.ended)
                 # kill the game
@@ -101,12 +104,22 @@ class Game:
         # print(to_delete)
         if len(to_delete) > 0:
             for i in range(len(to_delete)):
+                if self.col_available[0][self.asteroids[to_delete[i]][0]] > 0:
+                    self.col_available[0][self.asteroids[to_delete[i]][0]] -= 1
                 del self.asteroids[to_delete[i]]
         # print(self.asteroids)
         # generate asteroids
-        if self.r.uniform(0,1) > self.ast_thresh:
-            temp_col = self.r.randint(0,num_of_cols-1)
-            self.asteroids.append([temp_col, H])
+        if self.r.uniform(0,1) >= self.ast_thresh:
+            free_col = False
+            col_cnt = 0
+            while not free_col or col_cnt>num_of_cols:
+                temp_col = self.r.randint(0,num_of_cols-1)
+                if self.col_available[0][temp_col] < 2:
+                    self.asteroids.append([temp_col, H])
+                    self.col_available[0][temp_col] += 1
+                    free_col = True
+                else:
+                    col_cnt +=1
         # change speed if needed
         if (self.iter_cnt % 100) == 0:
             self.ast_thresh -= 0.05
@@ -117,7 +130,7 @@ class Game:
         
 
     def generateOutput(self):
-        out = np.ones((1, 41))
+        out = np.ones((1, num_of_cols))
         for asteroid in self.asteroids:
             if (out[0][asteroid[0]] > asteroid[1] / H):
                 out[0][asteroid[0]] = asteroid[1] / H
@@ -131,7 +144,7 @@ class Game:
             out2[0][half_out_width-i] = out[0][self.col-i]
         #rigth side
         for i in range(1,half_out_width+1):
-            if self.col+i>output_width-1:
+            if self.col+i>num_of_cols-1:
                 break
             out2[0][half_out_width+i] = out[0][self.col+i]
         #print(out2)
