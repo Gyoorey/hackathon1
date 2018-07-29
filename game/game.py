a2df4b9eda22c7792067bfaedc131070f5019f7a
Gyoorey <gyooorey@hotmail.com>
2018-07-29 17:07:38 +0200

    updated output format

--- game/game.py
+++ game/game.py
@@ -3,20 +3,23 @@ import random
 from random import Random
 import os
 import numpy as np
+import math
 
 H = 600
 W = 820
+num_of_cols = int(41) #should be even
 player_offset = 10
 player_y = -H / 2 + player_offset
 playerwidth = 30
-playerspeed = W / 41
+playerspeed = W / num_of_cols #one column
+output_width = int(num_of_cols) #should be even
+half_out_width = int((output_width-1)/2)
 
 
 class Game:
     def __init__(self):
         self.x = 0
-        self.screen = None
-        self.player = None
+        self.col = int(round((num_of_cols-1)/2))
         self.asteroids = []
         self.ast_thresh = 0.95
         self.iter_cnt = 0
@@ -24,46 +27,16 @@ class Game:
         self.r = Random(0)
         self.ended = False
 
-    def exit(self):
-        turtle.done()
-
     def show(self):
-        self.screen = turtle.Screen()
-        self.screen.setup(W + 20, H + 20)
-        self.screen.bgcolor("pink")
-        self.screen.title("Hackathon Invaders")
-        # draw border
-        border_pen = turtle.Turtle()
-        border_pen.speed(0)
-        border_pen.penup()
-        border_pen.setposition(-W / 2, H / 2)
-        border_pen.pendown()
-        border_pen.pensize(3)
-        border_pen.fd(W)
-        border_pen.rt(90)
-        border_pen.fd(H)
-        border_pen.rt(90)
-        border_pen.fd(W)
-        border_pen.rt(90)
-        border_pen.fd(H)
-        border_pen.hideturtle()
-        # draw player
-        self.player = turtle.Turtle()
-        self.player.color("black")
-        self.player.shape("triangle")
-        self.player.penup()
-        self.player.speed(0)
-        self.player.setposition(self.x, player_y)
-        self.player.setheading(90)
-        # draw asteroids
+        return 0
 
     def iterate(self, key):
         if (self.ended):
             return (self.iter_cnt, self.ended)
-        if key == -1 and self.x > -W / 2 + playerspeed:
-            self.x -= playerspeed
-        if key == 1 and self.x < W / 2 - playerspeed:
-            self.x += playerspeed
+        if key == -1 and self.col > 0:
+            self.col -= 1
+        if key == 1 and self.col < num_of_cols-1:
+            self.col += 1
         # move asteroids
         for i in range(len(self.asteroids)):
             self.asteroids[i][1] = self.asteroids[i][1] - self.ast_speed
@@ -86,29 +59,35 @@ class Game:
                 del self.asteroids[to_delete[i]]
         # print(self.asteroids)
         # generate asteroids
-        if self.r.random() > self.ast_thresh:
-            temp_x = self.r.random() * W
-            temp_x = round(temp_x / 20) * 20
-            temp_x -= W / 2
-            self.asteroids.append([temp_x, H / 2])
-            # self.asteroids[-1].color("red")
-            # self.asteroids[-1].shape("circle")
-            # self.asteroids[-1].penup()
-            # self.asteroids[-1].speed(0)
-            # self.asteroids[-1].setheading(270)
-            # self.asteroids[-1].setposition(temp_x, H/2)
+        if self.r.uniform(0,1) > self.ast_thresh:
+            temp_col = self.r.randint(0,num_of_cols)
+            self.asteroids.append([temp_col, H / 2])
         # change speed if needed
-        if (self.iter_cnt % 50) == 0:
-            self.ast_thresh -= 0.1
+        if (self.iter_cnt % 100) == 0:
+            self.ast_thresh -= 0.05
             #self.ast_speed += 5
         # incr. iter_cnt
         self.iter_cnt += 1
         return (self.iter_cnt, self.ended)
+        
 
     def generateOutput(self):
-        out = np.ones((1, 42))
+        out = np.ones((1, 41))
         for asteroid in self.asteroids:
-            if (out[0][round((asteroid[0] + W / 2) / 20)] > (asteroid[1] + H / 2) / H):
-                out[0][round((asteroid[0] + W / 2) / 20)] = (asteroid[1] + H / 2) / H
-        out[0][41] = (self.x * 2) / W
-        return out
+            if (out[0][asteroid[0]] > (asteroid[1] + H / 2) / H):
+                out[0][asteroid[0]] = (asteroid[1] + H / 2) / H
+        out2 = np.ones((1,output_width))*-1
+     
+        out2[0][half_out_width] = out[0][self.col]
+        #left side
+        for i in range(1,half_out_width+1):
+            if self.col-i<0:
+                break
+            out2[0][half_out_width-i] = out[0][self.col-i]
+        #rigth side
+        for i in range(1,half_out_width+1):
+            if self.col+i>output_width-1:
+                break
+            out2[0][half_out_width+i] = out[0][self.col+i]
+        #print(out2)
+        return out2
